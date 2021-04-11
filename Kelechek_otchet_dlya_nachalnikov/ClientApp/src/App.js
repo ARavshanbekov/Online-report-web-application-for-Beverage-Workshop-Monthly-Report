693c0b676b8edb6e28947677b1dc5987a6cbff18
Abdulaziz Ravshanbekov <ravshanbekov.abdulaziz@gmail.com>
2021-04-11 12:47:06 +0600

    CRU operations are added. Director permission and history table are left

--- Kelechek_otchet_dlya_nachalnikov/ClientApp/src/App.js
+++ Kelechek_otchet_dlya_nachalnikov/ClientApp/src/App.js
@@ -1,9 +1,7 @@
 // in src/App.js
 import * as React from "react";
-import { Admin, Resource, SelectInput, usePermissions } from 'react-admin';
+import { Admin, Resource, usePermissions } from 'react-admin';
 import { MemberList, MemberCreate, MemberEdit } from './pages/members';
-import { ReportList } from './pages/reports';
-import { TermoplastList, TermoplastCreate, TermoplastEdit, TermoplastShow } from './pages/termoplastIVyduv';
 import { CreateTsehTermoplast, EditTsehTermoplast, ShowTsehTermoplast, ListTsehTermoplast } from './pages/tsehTermoplast';
 import { ListTsehRozlivaSecond, CreateTsehRozlivaSecond, EditTsehRozlivaSecond, ShowTsehRozlivaSecond } from './pages/tsehRozlivaSecond';
 import { ListTsehRozlivaFirst, CreateTsehRozlivaFirst, EditTsehRozlivaFirst, ShowTsehRozlivaFirst } from './pages/tsehRozlivaFirst';
@@ -19,7 +17,7 @@ const i18nProvider = polyglotI18nProvider(() => russianMessages, 'ru');
 const App = () => {      
     const { permissions } = usePermissions();
     return (        
-        <Admin i18nProvider={i18nProvider} dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider} >
+        <Admin title="my page title" i18nProvider={i18nProvider} dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider} >
             {permissions => [
                 permissions === 'admin'
                     ? <Resource name="members" options={{ label: "Ползователи" }} list={MemberList} create={MemberCreate} edit={MemberEdit} />
@@ -34,7 +32,13 @@ const App = () => {
                     ? <Resource name="tsehRozlivaFirst" options={{ label: "Цех розлива №1" }} list={ListTsehRozlivaFirst} create={CreateTsehRozlivaFirst} edit={EditTsehRozlivaFirst} show={ShowTsehRozlivaFirst} />
                     : null,
                 permissions === 'chiefAccountant'
-                    ? <Resource name="termoplastIVyduv" options={{ label: "Термопласт и выдув" }} list={TermoplastList} show={TermoplastShow} />
+                    ? <Resource name="tsehRozlivaFirst" options={{ label: "Цех розлива №1" }} list={ListTsehRozlivaFirst} show={ShowTsehRozlivaFirst} />
+                    : null,
+                permissions === 'chiefAccountant'
+                    ? <Resource name="tsehRozlivaSecond" options={{ label: "Цех розлива №2" }} list={ListTsehRozlivaSecond} show={ShowTsehRozlivaSecond} />
+                    : null,
+                permissions === 'chiefAccountant'
+                    ? <Resource name="tsehTermoplast" options={{ label: "Цех розлива №2" }} list={ListTsehTermoplast} show={ShowTsehTermoplast} />
                     : null,
             ]}             
             </Admin >                       
